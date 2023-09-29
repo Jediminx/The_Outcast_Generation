5fdf40524a150522e611162be9297b987c625919
Padawan1997 <morningstar.luna@yahoo.com>
2023-09-29 13:12:53 -0400

    first commit

---
+++ javascript/Project.js
@@ -0,0 +1,44 @@
+const images = document.querySelectorAll('#slider .card');
+const previousImage = document.getElementById("prev");
+const nextImage = document.getElementById("next");
+
+let currentIndex = 0;
+
+function reset() {
+  for (let i = 0; i < images.length; i++) {
+    images[i].classList.remove('active');
+  }
+}
+
+function initializeSlider() {
+  reset();
+  images[currentIndex].classList.add('active');
+}
+
+function slideLeft() {
+  reset();
+  currentIndex--;
+  if (currentIndex < 0) {
+    currentIndex = images.length - 1;
+  }
+  images[currentIndex].classList.add('active');
+}
+
+function slideRight() {
+  reset();
+  currentIndex++;
+  if (currentIndex >= images.length) {
+    currentIndex = 0;
+  }
+  images[currentIndex].classList.add('active');
+}
+
+initializeSlider();
+
+previousImage.addEventListener('click', function() {
+  slideLeft();
+});
+
+nextImage.addEventListener('click', function() {
+  slideRight();
+});
